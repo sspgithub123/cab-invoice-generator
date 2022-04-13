9f2a9ef1dff0689c6ff9298918a5c278bcfd9d74
Shubham <ssp671995@gmail.com>
2022-04-14 02:23:08 +0530

    Added method to check rides from ride-repository and return the invoice

--- src/main/java/com/bridgelabz/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -1,5 +1,8 @@
 package com.bridgelabz;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author :Shubham Pawar
  * @version :1.0
@@ -42,6 +45,36 @@ public class CabInvoiceGenerator {
             totalFare += CalculateFare(ride.getDistance(), ride.getTime());
         }
         return new InvoiceSummary(rides.length, totalFare);
+    }
+
+    public InvoiceSummary getInvoice(int userId) {
+
+        Map<Integer, Ride[]> map = new HashMap<>();
+
+        Ride[] rides1 = {new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+
+        Ride[] rides2 = {new Ride(5.0, 10),
+                new Ride(1, 1)
+        };
+
+        Ride[] rides3 = {new Ride(8.0, 15),
+                new Ride(1, 10)
+        };
+
+        map.put(1, rides1);
+        map.put(2, rides2);
+        map.put(3, rides3);
+
+        for (Map.Entry<Integer, Ride[]> entry : map.entrySet()) {
+            if (userId == entry.getKey()) {
+                System.out.println(entry.getKey());
+                Ride[] ridesArray = entry.getValue();
+                return invoiceSummaryCalculation(ridesArray);
+            }
+        }
+        return null;
     }
 
 }
